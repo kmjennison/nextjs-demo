a9a8deaf58ff5313dcef804f03729d4364e87c3c
Kevin Jennison <kevin.jennison1@gmail.com>
2019-09-25 14:50:40 -0700

    Remove option incompatible with StaleWhileRevalidate

--- next.config.js
+++ next.config.js
@@ -16,7 +16,6 @@ const nextConfig = {
         handler: 'StaleWhileRevalidate',
         options: {
           cacheName: 'https-calls',
-          networkTimeoutSeconds: 15,
           expiration: {
             maxEntries: 150,
             maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
