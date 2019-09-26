d4d9038cab52baa9eeeb1c6d68046c8a31ba6b24
Kevin Jennison <kevin.jennison1@gmail.com>
2019-09-25 17:27:18 -0700

    Add an error page

---
+++ pages/_error.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Error = () => (
+  <div>
+    <Head>
+      <title>Error</title>
+    </Head>
+
+    <div>That's a problem!</div>
+  </div>
+)
+
+export default Error
